4f0de4ee700a90bf80e9ed21f5edd151b12fa7d7
rocabee-david <david@rocabee.com>
2017-08-12 22:36:39 +0100

    format code

--- index.js
+++ index.js
@@ -2,82 +2,82 @@
 const Map  = require('immutable').Map;
 
 function dataManipulation(entryObj, newEntry) {
-	let newObj = JSON.parse(JSON.stringify(entryObj));
-	let newEntryObj = convertEntryToComplicateObj(newEntry);
-	let newEntryObjPaths = getAllKeyPathes(newEntryObj);
-	let newKeys = [];
-	
-	newEntryObjPaths.map(path => {
-		let firstLevelKey = path.split('.')[1];
+    let newObj = JSON.parse(JSON.stringify(entryObj));
+    let newEntryObj = convertEntryToComplicateObj(newEntry);
+    let newEntryObjPaths = getAllKeyPathes(newEntryObj);
+    let newKeys = [];
+    
+    newEntryObjPaths.map(path => {
+        let firstLevelKey = path.split('.')[1];
 
-		if (!newObj[firstLevelKey]) {
-			newObj[firstLevelKey] = newEntryObj[firstLevelKey];
-			newKeys.push(firstLevelKey);
-			return ;
-		}
+        if (!newObj[firstLevelKey]) {
+            newObj[firstLevelKey] = newEntryObj[firstLevelKey];
+            newKeys.push(firstLevelKey);
+            return ;
+        }
 
-		if (newKeys.indexOf(firstLevelKey) !== -1) {
-			return ;
-		}
+        if (newKeys.indexOf(firstLevelKey) !== -1) {
+            return ;
+        }
 
-		let calcStr = `newObj${path} = newObj${path} + newEntryObj${path}`;
-		
-		try {
-			eval(calcStr);
-		} catch (e) {
-			console.log('error', e.message);
-		}
-	});
+        let calcStr = `newObj${path} = newObj${path} + newEntryObj${path}`;
+        
+        try {
+            eval(calcStr);
+        } catch (e) {
+            console.log('error', e.message);
+        }
+    });
 
-	return newObj;
+    return newObj;
 }
 
 function convertEntryToComplicateObj(entry) {
-	let obj = {};
-	let numbericObj = {count: 1};
-	let entryKies = Object.keys(entry);
+    let obj = {};
+    let numbericObj = {count: 1};
+    let entryKies = Object.keys(entry);
 
-	entryKies.map(k => {
-		if (typeof entry[k] === 'number') {
-			let key = `${k}_total`;
-			obj[key] = entry[k];
-			numbericObj[key] = entry[k];
-		}
-	});
+    entryKies.map(k => {
+        if (typeof entry[k] === 'number') {
+            let key = `${k}_total`;
+            obj[key] = entry[k];
+            numbericObj[key] = entry[k];
+        }
+    });
 
-	entryKies.map(k => {
-		if (typeof entry[k] === 'string') {
-			obj[k] = {};
-			obj[k][entry[k]] = Object.assign({}, numbericObj);
-		}
-	});
+    entryKies.map(k => {
+        if (typeof entry[k] === 'string') {
+            obj[k] = {};
+            obj[k][entry[k]] = Object.assign({}, numbericObj);
+        }
+    });
 
-	obj.count = 1;
+    obj.count = 1;
 
-	return obj;
+    return obj;
 }
 
 function getAllKeyPathes(obj, preFixPath) {
-	if (!preFixPath) {
-		preFixPath = '';
-	}
-	
-	let keys = Object.keys(obj);
-	let paths = [];
-	
-	keys.map(k => {
-		if (typeof obj[k] === 'object') {
-			paths = paths.concat(getAllKeyPathes(obj[k], preFixPath + '.' + k));
-		} else {
-			paths.push(preFixPath + '.' + k);
-		}
-	});
+    if (!preFixPath) {
+        preFixPath = '';
+    }
+    
+    let keys = Object.keys(obj);
+    let paths = [];
+    
+    keys.map(k => {
+        if (typeof obj[k] === 'object') {
+            paths = paths.concat(getAllKeyPathes(obj[k], preFixPath + '.' + k));
+        } else {
+            paths.push(preFixPath + '.' + k);
+        }
+    });
 
-	return paths;
+    return paths;
 }
 
 module.exports = {
-	dataManipulation,
-	convertEntryToComplicateObj,
-	getAllKeyPathes
+    dataManipulation,
+    convertEntryToComplicateObj,
+    getAllKeyPathes
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -41,83 +41,83 @@ const entry = {
 };
 
 describe('data manipulation', () => {
-	it('when all the key already exist', () => {
-		let newObj =  dataService.dataManipulation(baseObj, entry);
-		let expectedObj = { count: 16,
-		  amount_total: 442,
-		  num_items_total: 5,
-		  gender:
-		   { F: { count: 5, amount_total: 225, num_items_total: 3 },
-		     M: { count: 11, amount_total: 217, num_items_total: 7 } },
-		  currency:
-		   { EUR: { count: 14, amount_total: 192, num_items_total: 8 },
-		     USD: { count: 2, amount_total: 250, num_items_total: 6 } } 
-		};
-		
-		expect(newObj).to.deep.equal(expectedObj);
-	});
+    it('when all the key already exist', () => {
+        let newObj =  dataService.dataManipulation(baseObj, entry);
+        let expectedObj = { count: 16,
+          amount_total: 442,
+          num_items_total: 5,
+          gender:
+           { F: { count: 5, amount_total: 225, num_items_total: 3 },
+             M: { count: 11, amount_total: 217, num_items_total: 7 } },
+          currency:
+           { EUR: { count: 14, amount_total: 192, num_items_total: 8 },
+             USD: { count: 2, amount_total: 250, num_items_total: 6 } } 
+        };
+        
+        expect(newObj).to.deep.equal(expectedObj);
+    });
 
-	it('when key not exsit in original object', () => {
-		let newEntry = {
-			"weekday": "monday",
-			"amount": 17.0,
-   			"num_items": 2,
-   			"currency": "EUR"
-		};
+    it('when key not exsit in original object', () => {
+        let newEntry = {
+            "weekday": "monday",
+            "amount": 17.0,
+            "num_items": 2,
+            "currency": "EUR"
+        };
 
-		let newObj =  dataService.dataManipulation(baseObj, newEntry);
-		
-		let expectedObj = { 
-			count: 16,
-			amount_total: 442,
-			num_items_total: 5,
-			gender:
-			 { F: { count: 5, amount_total: 225, num_items_total: 3 },
-			   M: { count: 10, amount_total: 200, num_items_total: 5 } },
-			currency:
-			 { EUR: { count: 14, amount_total: 192, num_items_total: 8 },
-			   USD: { count: 2, amount_total: 250, num_items_total: 6 } },
-			weekday: { monday: { count: 1, amount_total: 17, num_items_total: 2 } } 
-		};
+        let newObj =  dataService.dataManipulation(baseObj, newEntry);
+        
+        let expectedObj = { 
+            count: 16,
+            amount_total: 442,
+            num_items_total: 5,
+            gender:
+             { F: { count: 5, amount_total: 225, num_items_total: 3 },
+               M: { count: 10, amount_total: 200, num_items_total: 5 } },
+            currency:
+             { EUR: { count: 14, amount_total: 192, num_items_total: 8 },
+               USD: { count: 2, amount_total: 250, num_items_total: 6 } },
+            weekday: { monday: { count: 1, amount_total: 17, num_items_total: 2 } } 
+        };
 
-		expect(expectedObj).to.deep.equal(newObj);		
-	});
+        expect(expectedObj).to.deep.equal(newObj);      
+    });
 
-  	it('convertEntryToComplicateObj', () => {
-		let newObj = dataService.convertEntryToComplicateObj(entry);
-		
-		let expectedObj = { 
-			amount_total: 17,
-  			num_items_total: 2,
-  			gender: { M: { amount_total: 17, num_items_total: 2 , count: 1} },
-  			currency: { EUR: { amount_total: 17, num_items_total: 2, count: 1} },
-  			count: 1 
-  		};
-		
-		expect(newObj).to.deep.equal(expectedObj);
-  	});
+    it('convertEntryToComplicateObj', () => {
+        let newObj = dataService.convertEntryToComplicateObj(entry);
+        
+        let expectedObj = { 
+            amount_total: 17,
+            num_items_total: 2,
+            gender: { M: { amount_total: 17, num_items_total: 2 , count: 1} },
+            currency: { EUR: { amount_total: 17, num_items_total: 2, count: 1} },
+            count: 1 
+        };
+        
+        expect(newObj).to.deep.equal(expectedObj);
+    });
 
-  	it('getAllKeyPathes', () => {
-  		let paths = dataService.getAllKeyPathes(baseObj);
-  		let expectedPaths = [ 
-  		  '.count',
-		  '.amount_total',
-		  '.num_items_total',
-		  '.gender.F.count',
-		  '.gender.F.amount_total',
-		  '.gender.F.num_items_total',
-		  '.gender.M.count',
-		  '.gender.M.amount_total',
-		  '.gender.M.num_items_total',
-		  '.currency.EUR.count',
-		  '.currency.EUR.amount_total',
-		  '.currency.EUR.num_items_total',
-		  '.currency.USD.count',
-		  '.currency.USD.amount_total',
-		  '.currency.USD.num_items_total' 
-		];
+    it('getAllKeyPathes', () => {
+        let paths = dataService.getAllKeyPathes(baseObj);
+        let expectedPaths = [ 
+          '.count',
+          '.amount_total',
+          '.num_items_total',
+          '.gender.F.count',
+          '.gender.F.amount_total',
+          '.gender.F.num_items_total',
+          '.gender.M.count',
+          '.gender.M.amount_total',
+          '.gender.M.num_items_total',
+          '.currency.EUR.count',
+          '.currency.EUR.amount_total',
+          '.currency.EUR.num_items_total',
+          '.currency.USD.count',
+          '.currency.USD.amount_total',
+          '.currency.USD.num_items_total' 
+        ];
 
-		// console.log('paths', paths);
-		expect(paths).to.deep.equal(expectedPaths);  		
-  	});
+        // console.log('paths', paths);
+        expect(paths).to.deep.equal(expectedPaths);         
+    });
 });
